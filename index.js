b089b9f3c0dd09817d56ecb54142ead9703775ca
jmunozl <jos.munozl@gmail.com>
2020-09-05 18:51:32 -0400

    Se modifica glosa index

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.listen(5000,()=>{
-  console.log('Servidor corriendo')
+  console.log('Servidor se encuentra ejecutando en el puerto: 5000' )
 })
